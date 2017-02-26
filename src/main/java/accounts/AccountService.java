d8f3e1062b1d7a505424a25869e20e0d82e3e6d1
Sasha Abdulova <sanchoponchos@gmail.com>
2017-02-26 21:16:57 +0600

    UserProfile - описывает профиль пользователя. AccountService - описывает залогиненных и авторизованных пользователей

---
+++ src/main/java/accounts/AccountService.java
@@ -0,0 +1,38 @@
+package accounts;
+
+import org.eclipse.jetty.server.Authentication;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AccountService {
+    private Map<String, UserProfile> loginToProfile;
+    private Map<String, UserProfile> sessionIdToProfile;
+
+    public AccountService() {
+        loginToProfile = new HashMap<String, UserProfile>();
+        sessionIdToProfile = new HashMap<String, UserProfile>();
+    }
+
+    public void addNewUser(UserProfile userProfile) {
+        loginToProfile.put(userProfile.getLogin(), userProfile);
+    }
+
+    public UserProfile getUserByLogin (String login) {
+        return loginToProfile.get(login);
+    }
+
+    public UserProfile getUserBySession (String sessionId) {
+        return sessionIdToProfile.get(sessionId);
+    }
+
+    public void addSession(String sessionId, UserProfile userProfile) {
+        sessionIdToProfile.put(sessionId,userProfile);
+    }
+
+    public void deleteSession(String sessionId) {
+        sessionIdToProfile.remove(sessionId);
+    }
+
+
+}
